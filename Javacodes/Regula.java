f7713e82cfb54d6456599787722e3857ca8fff3e
Denis <andridenis@gmail.com>
2017-06-20 23:23:09 +0200

    Regula modified

--- Javacodes/Regula.java
+++ Javacodes/Regula.java
@@ -8,7 +8,6 @@ public class Regula {
 	public double execute(double a, double b, double e) {
 		double x = a;
 		double x1;
-		
 
 		do {
 			x1 = x;
@@ -19,17 +18,49 @@ public class Regula {
 
 			else
 				a = x;
-			System.out.println(a+" "+b+" "+f(a)+" "+f(b)+" "+x+" "+x1+" "+ Math.abs(x-x1)+" "+ e );
+			System.out.println(
+					a + " " + b + " " + f(a) + " " + f(b) + " " + x + " " + x1 + " " + Math.abs(x - x1) + " " + e);
 
 		} while (Math.abs(x - x1) >= e);
 
 		return x;
 	}
 
+	public double modExecute(double a, double b, double e) {
+		double F = f(a);
+		double G = f(b);
+		double x1;
+		double x = a;
+
+		do {
+			x1 = x;
+			x = (G * a - F * b) / (G - F);
+
+			if (f(a) * f(b) * f(x) <= 0) {
+				b = x;
+				G = f(x);
+				if (f(x) * f(x1) > 0)
+					F = F / 2;
+
+			} else {
+				a = x;
+				F = f(x);
+				if (f(x) * f(x1) > 0)
+					G = G / 2;
+			}
+			System.out.println(
+					a + " " + b + " " + f(a) + " " + f(b) + " " + x + " " + x1 + " " + Math.abs(x - x1) + " " + e);
+
+		} while (Math.abs(x - x1) > e);
+		return x;
+	}
+
 	public static void main(String[] args) {
-		
+
 		Regula regula = new Regula();
-		regula.execute(0.5,10, Math.pow(10, -9));
+		regula.execute(0.5, 10, Math.pow(10, -9));
+		System.out.println();
+		regula.modExecute(0.5, 10, Math.pow(10, -9));
 
 	}
 
